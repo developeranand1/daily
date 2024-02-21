6968a8f8f86f4fa76788e93c95719b2a669c2b33
Anand Prakash Chaudhary <apchaudhary8126@gmail.com>
2024-02-21 17:20:58 +0530

    first

---
+++ day15/app.js
@@ -0,0 +1,4 @@
+// const num :number=10;
+var named;
+named = "8767378";
+console.log(named = 222);
---
+++ day15/app.ts
@@ -0,0 +1,4 @@
+// const num :number=10;
+let named:string;
+named="8767378";
+console.log(named=222);
